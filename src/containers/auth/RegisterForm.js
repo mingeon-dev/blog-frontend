b1eb6ff8db0014b6a5774c106a35dddc087417df
MinGeon Kim <mingeon.dev@gmail.com>
2020-03-19 08:09:42 +0900

    feat: localStorage를 사용한 로그인 상태 유지, 로그아웃 기능 구현

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -63,6 +63,11 @@ const RegisterForm = ({ history }) => {
   useEffect(() => {
     if (user) {
       history.push('/');
+      try {
+        localStorage.setItem('user', JSON.stringify(user));
+      } catch (e) {
+        console.log('localStorage is not working');
+      }
     }
   }, [history, user]);
 
