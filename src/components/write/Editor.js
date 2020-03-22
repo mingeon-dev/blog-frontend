48ba89d3721d66f0c65b270529316e2458c7c7ff
MinGeon Kim <mingeon.dev@gmail.com>
2020-03-23 07:57:23 +0900

    feat: 에디터 화면에 리덕스 적용 및 API 연동

--- src/components/write/Editor.js
+++ src/components/write/Editor.js
@@ -30,7 +30,7 @@ const QuillWrapper = styled.div`
   }
 `;
 
-const Editor = () => {
+const Editor = ({ title, body, onChangeField }) => {
   const quillElement = useRef(null);
   const quillInstance = useRef(null);
 
@@ -47,11 +47,26 @@ const Editor = () => {
         ],
       },
     });
-  }, []);
+
+    const quill = quillInstance.current;
+    quill.on('text-change', (delta, oldDelta, source) => {
+      if (source === 'user') {
+        onChangeField({ key: 'body', value: quill.root.innerHTML });
+      }
+    });
+  }, [onChangeField]);
+
+  const onChangeTitle = e => {
+    onChangeField({ key: 'title', value: e.target.value });
+  };
 
   return (
     <EditorBlock>
-      <TitleInput placeholder="제목을 입력하세요" />
+      <TitleInput
+        onChange={onChangeTitle}
+        value={title}
+        placeholder="제목을 입력하세요"
+      />
       <QuillWrapper>
         <div ref={quillElement} />
       </QuillWrapper>
